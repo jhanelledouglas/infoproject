57440545d0fae94fa8e9ac817a69d51899d87dc4
jhanxo <38596838+jhanxo@users.noreply.github.com>
2021-03-23 22:44:36 -0500

    commit

--- app/config.py
+++ app/config.py
@@ -3,8 +3,12 @@ import os
 class Config(object):
     """Base Config Object"""
     DEBUG = False
+
     SECRET_KEY ='Som3$ec5etK*y'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+    # rest of connection code using the connection string `uri`
     #SQLALCHEMY_DATABASE_URI='postgresql://info:monique@localhost/info'
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
     UPLOAD_FOLDER = './app/static/img'
